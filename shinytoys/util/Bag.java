ccbc94ccbd96197508e3e3166c6556c0af0a2da0
Izzac Shelton <IzzacShelton@gmail.com>
2025-09-17 13:59:52 -0500

    create Bag class  # Please enter the commit message for your changes. Lines starting

---
+++ shinytoys/util/Bag.java
@@ -0,0 +1,38 @@
+import java.util.HashMap;
+
+/**
+ * Simple 'Bag' class which counts the number of times each unique item is put into it. (e.g. counting occurences of words in text)
+ */
+public class Bag<E> {
+   private HashMap<E, Integer> counts = new HashMap<>();
+   public Bag() {}
+
+   /**
+    * retrieves the bag's count for the specified item
+    * @param item
+    * @return the number of times the item has been put into the bag.
+    */
+   public int get(E item) {
+      if (counts.containsKey(item)) {
+         return counts.get(item);
+      } else {
+         return 0;
+      }
+   }
+   
+   /**
+    * increments/initializes the count for the given item
+    * @param item
+    * @return the previous count
+    */
+   public int put(E item) {
+      int prev = counts.containsKey(item) ? counts.get(item) : 0;
+      counts.put(item, prev+1);
+      return prev;
+   }
+
+   @Override
+   public String toString() {
+      return counts.toString();
+   }
+} 
